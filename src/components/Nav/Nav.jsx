89ab5cf3d3d52387c5a25a4ee0c4804b25208b42
JacobMarineau <jacobmarineaump@gmail.com>
2024-10-24 14:42:58 -0500

    Updated nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,62 +1,89 @@
+/** @jsxImportSource @emotion/react */
 import React from 'react';
 import { Link } from 'react-router-dom';
-import LogOutButton from '../LogOutButton/LogOutButton';
-import './Nav.css';
 import { useSelector } from 'react-redux';
+import { css } from '@emotion/react';
+import LogOutButton from '../LogOutButton/LogOutButton';
+import { theme } from '../../theme/theme'; 
 
 function Nav({ characterId }) {
   const user = useSelector((store) => store.user);
   const userId = useSelector((store) => store.user.id);
 
+  // Primary Nav Styling
+  const primaryNavStyle = css`
+    background-color: ${theme.colors.baseColor};
+    padding: ${theme.spacing.medium};
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    box-shadow: 0px 4px 10px ${theme.colors.boxShadow};
+    position: relative;
+    z-index: 10;
+  `;
+
+  // Secondary Nav Styling
+  const secondaryNavStyle = css`
+    background-color: ${theme.colors.baseColor2};
+    padding: ${theme.spacing.small};
+    display: flex;
+    justify-content: space-around;
+    margin-top: ${theme.spacing.small};
+  `;
+
+  // Link Styling for Primary Nav
+  const linkStylePrimary = css`
+    color: ${theme.colors.offWhiteBackground2};
+    margin-right: ${theme.spacing.medium};
+    font-size: ${theme.typography.sizes.large};
+    font-family: ${theme.fonts.heading};
+    padding: 4px;
+    border-radius: 4px;
+    text-decoration: none;
+    &:hover {
+      color: ${theme.colors.accents};
+    }
+    box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.5); /* Drop shadow */
+  `;
+
+  // Link Styling for Secondary Nav
+  const linkStyleSecondary = css`
+    color: ${theme.colors.offWhiteBackground};
+    text-decoration: none;
+    font-size: ${theme.typography.sizes.normal};
+    &:hover {
+      color: ${theme.colors.trim};
+    }
+  `;
+
   return (
-    <div className="nav">
-      <Link to="/home">
-        <h2 className="nav-title">Prime Solo Project</h2>
-      </Link>
-      <div>
-        {!user.id && (
-          <Link className="navLink" to="/login">
-            Login / Register
-          </Link>
-        )}
+    <>
+      {/* Primary Nav */}
+      <div css={primaryNavStyle}>
+        {/* Space for logo */}
+        <h2 className="nav-title" css={linkStylePrimary}>Prime Solo Project</h2>
 
         {user.id && (
           <>
-            <Link className="navLink" to="/user">
+            <Link css={linkStylePrimary} to="/home">
               Home
             </Link>
-
-            <Link className="navLink" to="/info">
-              Info Page
-            </Link>
-
-            <LogOutButton className="navLink" />
+            <LogOutButton css={linkStylePrimary} />
           </>
         )}
-
-        <Link className="navLink" to="/characterstats1">
-          Character Creator
-        </Link>
-
-        <Link className="navLink" to="/characterstats2">
-          Character Creator Part 2
-        </Link>
-
-        <Link className="navLink" to="/characterstats3">
-          Character Creator Part 3
-        </Link>
-
-        <Link className="navLink" to={`/character-summary/${userId}`}>
-          Characters
-        </Link>
-
-       
-              <Link className="navLink" to={`/character-sheet/${characterId}`}>
-                Char Sheet
-              </Link>
-
       </div>
-    </div>
+
+      {/* Secondary Nav */}
+      {user.id && (
+        <div css={secondaryNavStyle}>
+          <Link css={linkStyleSecondary} to="/characterstats1">Character Creator</Link>
+          <Link css={linkStyleSecondary} to="/characterstats2">Character Creator Part 2</Link>
+          <Link css={linkStyleSecondary} to="/characterstats3">Character Creator Part 3</Link>
+          <Link css={linkStyleSecondary} to={`/character-summary/${userId}`}>Characters</Link>
+          <Link css={linkStyleSecondary} to={`/character-sheet/${characterId}`}>Char Sheet</Link>
+        </div>
+      )}
+    </>
   );
 }
 
